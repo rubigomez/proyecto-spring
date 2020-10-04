ab1c21b7ae62125a8e00aa92f3c9315f88258bef
Rubi <gomr920604@gmail.com>
2020-10-04 00:50:50 -0500

    Primer version del proyecto Spring boot

---
+++ src/main/java/com/upemor/sesioncuatro/service/RoleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.upemor.sesioncuatro.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.upemor.sesioncuatro.model.Role;
+import com.upemor.sesioncuatro.repository.RoleRepository;
+
+@Service
+public class RoleServiceImpl {
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	
+	public void createRole (Role role) {
+		
+		roleRepository.save(role);
+	}
+
+}
